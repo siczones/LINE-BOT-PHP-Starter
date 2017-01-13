b5394cf458d11b0c9c4214761c968e7c9ed16481
Siczones <jirapan-sic@hotmail.com>
2017-01-13 13:39:50 +0700

    test

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $replyToken
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -44,4 +44,5 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK";
+$botname = "siczones-bot";
+echo "OK" . $botname;
