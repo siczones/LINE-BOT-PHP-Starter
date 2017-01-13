a8d51111b6c5b45677080cc6f973c9c89e9ce7f5
Siczones <jirapan-sic@hotmail.com>
2017-01-13 15:35:56 +0700

    $botname = "siczones-bot";

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text . $botname
+				'text' => $text . " จาก ". $botname
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
