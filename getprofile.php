e4cadf3b7d04c26e599ef27f31fa45fbc1cb2e87
Siczones <jirapan-sic@hotmail.com>
2017-01-14 22:11:44 +0700

    ss

--- getprofile.php
+++ getprofile.php
@@ -8,18 +8,29 @@
 $access_token = 'hb8oE3kH7ys+kpqrUcKCEeAii6gvsAmf4hKTJzffNe6VEkcptKpdczTKs7BHuxlkN3JbH8731E1D7/h/4Lu2L5gjKrTTW9kpwGTcZd7w+tpw/RPQEWWILrhfePT0s2nUe2M+O50e1NOPVUpNAF3emwdB04t89/1O/w1cDnyilFU=';
 $user_id = 'siczones';
 
-$url = 'https://api.line.me/v2/bot/profile/' .$user_id;
+// $url = 'https://api.line.me/v2/bot/profile/' .$user_id;
 
-$headers = array('Authorization: Bearer ' . $access_token);
+// $headers = array('Authorization: Bearer ' . $access_token);
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+// $ch = curl_init($url);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+// $result = curl_exec($ch);
+// curl_close($ch);
 
-echo "<h2>This is Push message and JSON file from Sever </h2>" .$result;
+// echo "<h2>This is Push message and JSON file from Sever </h2>" .$result;
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '4fa72238672c25a970d378eb364ac3af']);
+$response = $bot->getProfile($user_id);
+if ($response->isSucceeded()) {
+    $profile = $response->getJSONDecodedBody();
+    echo $profile['displayName'];
+    echo $profile['pictureUrl'];
+    echo $profile['statusMessage'];
+}
+
 ?>
 
 </body>
