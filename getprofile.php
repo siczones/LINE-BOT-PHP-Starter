2739af4b0febc0a9d99d9711e3b87b64abaaf56d
Siczones <jirapan-sic@hotmail.com>
2017-01-14 22:07:29 +0700

    update Get profile

--- getprofile.php
+++ getprofile.php
@@ -6,7 +6,7 @@
 </head>
 <?php
 $access_token = 'hb8oE3kH7ys+kpqrUcKCEeAii6gvsAmf4hKTJzffNe6VEkcptKpdczTKs7BHuxlkN3JbH8731E1D7/h/4Lu2L5gjKrTTW9kpwGTcZd7w+tpw/RPQEWWILrhfePT0s2nUe2M+O50e1NOPVUpNAF3emwdB04t89/1O/w1cDnyilFU=';
-$user_id = 'siczones'
+$user_id = 'siczones';
 
 $url = 'https://api.line.me/v2/bot/profile/' .$user_id;
 
