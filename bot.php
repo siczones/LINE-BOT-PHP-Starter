ad1c110b579ee621f052394fa77784d2d6786586
Siczones <jirapan-sic@hotmail.com>
2017-01-14 14:26:31 +0700

    double data

--- bot.php
+++ bot.php
@@ -31,6 +31,7 @@ if (!is_null($events['events'])) {
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
