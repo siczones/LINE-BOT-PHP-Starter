c6c4c85e9dde7702e83be7ef33ca0efb4ead05b5
Siczones <jirapan-sic@hotmail.com>
2017-01-14 22:06:05 +0700

    added Get profile

---
+++ getprofile.php
@@ -0,0 +1,26 @@
+<!-- Get profile -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LINE-Bot php Get profile</title>	
+</head>
+<?php
+$access_token = 'hb8oE3kH7ys+kpqrUcKCEeAii6gvsAmf4hKTJzffNe6VEkcptKpdczTKs7BHuxlkN3JbH8731E1D7/h/4Lu2L5gjKrTTW9kpwGTcZd7w+tpw/RPQEWWILrhfePT0s2nUe2M+O50e1NOPVUpNAF3emwdB04t89/1O/w1cDnyilFU=';
+$user_id = 'siczones'
+
+$url = 'https://api.line.me/v2/bot/profile/' .$user_id;
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo "<h2>This is Push message and JSON file from Sever </h2>" .$result;
+?>
+
+</body>
+</html>
