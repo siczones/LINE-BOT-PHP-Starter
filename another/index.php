9f19878c0f624d0385b83aeed318ca71e9b91f33
Siczones <jirapan-sic@hotmail.com>
2017-01-18 00:11:59 +0700

    กกก

--- another/index.php
+++ another/index.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/lineBot.php';
 
 $bot = new Linebot();
 $text = $bot->getMessageText();
-$bot->reply($text);
+$bot->reply($text .ก็ได้);
 
 echo "<br><h1>This LINE-bot is working now!</h1>";
 
