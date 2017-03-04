9a746aad68b4da529ae81ceb6b6a40e9e3d9355d
Siczones <jirapan-sic@hotmail.com>
2017-03-04 16:20:36 +0700

    add push and reply

--- another/index.php
+++ another/index.php
@@ -14,6 +14,22 @@ elseif( ($text == 'เปิดไฟ') or ($text == 'ปิดไฟ') ){
 }
 elseif( ($text == 'อุณหภูมิ')){
 	$bot->reply('ระบบกำลังตรวจสอบ...' .$text .'   ให้ท่านอยู่ '.' กรุณารอสักครู่! ในระหว่างนี้ท่านสามารถตรวจสอบสถานะการทำงานของระบบได้ที่ ' .'https://siczones.coe.psu.ac.th');	
+	$url = 'https://siczones.coe.psu.ac.th/cgi-bin/requestAlert.py';
+	$data = array('ID' => 'temp', 'key' => 'abcd');
+	
+	// use key 'http' even if you send the request to https://...
+	$options = array(
+	    'http' => array(
+	        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+	        'method'  => 'POST',
+	        'content' => http_build_query($data)
+	    )
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	if ($result === FALSE) { /* Handle error */ }
+	
+	var_dump($result);
 }
 elseif( ($text == 'ความชื้น')){
 	$bot->reply('ระบบกำลังตรวจสอบ...' .$text .'   ให้ท่านอยู่ '.' กรุณารอสักครู่! ในระหว่างนี้ท่านสามารถตรวจสอบสถานะการทำงานของระบบได้ที่ ' .'https://siczones.coe.psu.ac.th');	
