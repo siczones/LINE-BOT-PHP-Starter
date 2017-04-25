0d264b8ed0adc3e7abeff9d9549161318759e716
Siczones <jirapan-sic@hotmail.com>
2017-04-25 17:29:10 +0700

    e

--- new_line_mode/test.php
+++ new_line_mode/test.php
@@ -9,8 +9,6 @@ $apiReply = Setting::getApiReply();
 $apiPush = Setting::getApiPush();
 
 echo "<br><h1>This LINE-bot is working now!</h1>";
-echo "<br>Channel access token is</h1>" .$channelAccessToken;
-
 echo "@" .date('Y-m-d H:i:s'); 
 echo "<hr><h3>success</h3><hr>";
 
